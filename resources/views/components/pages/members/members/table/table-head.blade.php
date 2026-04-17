aa9859e39543546503b004a72ed7fc39f07a8375
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2026-04-17 20:12:26 +0200

    fix column filter

--- resources/views/components/pages/members/members/table/table-head.blade.php
+++ resources/views/components/pages/members/members/table/table-head.blade.php
@@ -1,5 +1,5 @@
 <thead>
-    <tr x-data="{ name: 'middle', email: 'middle', role: 'middle' }">
+    <tr x-data="{ name: 'middle', email: 'middle' }">
         <th>
             <div class="flex items-center justify-center">
                 <input :checked="$wire.selectedColumn.length > 0"
@@ -27,15 +27,12 @@
         <th>
             <button type="button" class="flex items-center gap-2" @click="
             if (name === 'middle') {
-                    role = 'middle';
                     email = 'middle';
                     name = 'desc'
                 } else if (name === 'desc') {
-                     role = 'middle';
                     email = 'middle';
                     name = 'asc'
                 } else if (name === 'asc') {
-                     role = 'middle';
                     email = 'middle';
                     name = 'middle'
                 }
@@ -55,15 +52,12 @@
         <th>
             <button type="button" class="flex items-center gap-2" @click="
             if (email === 'middle') {
-                    role = 'middle';
                     name = 'middle';
                     email = 'desc'
                 } else if (email === 'desc') {
-                     role = 'middle';
                     name = 'middle';
                     email = 'asc'
                 } else if (email === 'asc') {
-                     role = 'middle';
                     name = 'middle';
                     email = 'middle'
                 }
@@ -84,32 +78,7 @@
             <span>{{ __('tables.phone') }}</span>
         </th>
         <th>
-            <button type="button" class="flex items-center gap-2" @click="
-            if (role === 'middle') {
-                    email = 'middle';
-                    name = 'middle';
-                    role = 'desc'
-                } else if (role === 'desc') {
-                     email = 'middle';
-                    name = 'middle';
-                    role = 'asc'
-                } else if (role === 'asc') {
-                     email = 'middle';
-                    name = 'middle';
-                    role = 'middle'
-                }
-                $wire.sortBy('role', role)">
-                <span>{{ __('tables.role') }}</span>
-                <svg :class="{
-                            'rotate-0': role === 'desc',
-                            '-rotate-180': role === 'asc',
-                            '-rotate-90': role === 'middle'
-                         }"
-                     class="trans-all" width="11" height="16" viewBox="0 0 11 16" fill="none"
-                     xmlns="http://www.w3.org/2000/svg">
-                    <use href="#th-filter-arrow"></use>
-                </svg>
-            </button>
+            <span>{{ __('tables.role') }}</span>
         </th>
         <th>
             <span>{{ __('tables.status') }}</span>
