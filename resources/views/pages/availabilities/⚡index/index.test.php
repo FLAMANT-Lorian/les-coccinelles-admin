01079779c94cdd8da893cc2ac18ea45e20c943a8
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2026-05-04 18:36:48 +0200

    test: Added test for availability requests index

--- resources/views/pages/availabilities/⚡index/index.test.php
+++ resources/views/pages/availabilities/⚡index/index.test.php
@@ -1,1 +1,52 @@
 <?php
+
+use App\Models\User;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+
+describe('VIEW AVAILABILITY REQUESTS WITH PERMISSIONS', function () {
+    beforeEach(function () {
+        $permission = Permission::create([
+            'name' => 'availabilities.index',
+            'guard_name' => 'web',
+        ]);
+
+        $role = Role::create([
+            'name' => 'Test',
+            'guard_name' => 'web',
+            'unique' => 1
+        ]);
+
+        $role->givePermissionTo($permission);
+        $this->user = User::factory()->create();
+        $this->user->assignRole($role);
+        $this->actingAs($this->user);
+    });
+
+    it('verifies if a user with the permission can access to the availability requests index', function () {
+        $this->get(route('availabilities', ['locale' => config('app.locale')]))
+            ->assertOk();
+    });
+});
+
+describe('VIEW AVAILABILITY REQUESTS WITHOUT PERMISSIONS', function () {
+    beforeEach(function () {
+        $role = Role::create([
+            'name' => 'Test',
+            'guard_name' => 'web',
+            'unique' => 1
+        ]);
+
+        $this->user = User::factory()->create();
+
+        $this->user->assignRole($role);
+
+        $this->actingAs($this->user);
+    });
+
+    it('verifies if a user without the permission can’t access to the availability requests index', function () {
+        $this->get(route('availabilities', ['locale' => config('app.locale')]))
+            ->assertForbidden();
+    });
+});
