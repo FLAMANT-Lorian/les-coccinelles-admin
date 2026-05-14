62260351416b846b7c11c0a2acacedae407c0777
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2026-05-14 14:31:15 +0200

    test: Correcting tests because the migrations have changed

--- resources/views/pages/bookings/⚡update/update.test.php
+++ resources/views/pages/bookings/⚡update/update.test.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Models\Booking;
+use App\Models\MeterReading;
 use Livewire\Livewire;
 
 it('renders successfully', function () {
-    Livewire::test('pages::bookings.update')
+    $booking = Booking::factory()
+        ->has(MeterReading::factory())
+        ->create();
+
+    Livewire::test('pages::bookings.update', ['booking' => $booking])
         ->assertStatus(200);
 });
