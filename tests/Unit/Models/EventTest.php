9a00056bbd7d99dfda6d93faa10c330e463d4f9a
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2026-05-28 18:31:28 +0200

    feat: Made database for tasks

--- tests/Unit/Models/EventTest.php
+++ tests/Unit/Models/EventTest.php
@@ -2,6 +2,8 @@
 
 use App\Models\Event;
 use App\Models\Folder;
+use App\Models\Task;
+use App\Models\User;
 use function Pest\Laravel\assertDatabaseCount;
 
 it('tests relations between events and folders', function () {
@@ -12,9 +14,16 @@ it('tests relations between events and folders', function () {
         ->for($event)
         ->create();
 
+    $task = Task::factory()
+        ->for($event)
+        ->assignedTo(User::factory()->create())
+        ->create();
+
     assertDatabaseCount('events', 1);
     assertDatabaseCount('folders', 1);
 
     expect($event->folders()->first()->name)->toBe($folder->name)
-        ->and($event->name)->toBe($folder->event->name);
+        ->and($event->name)->toBe($folder->event->name)
+        ->and($task->event->name)->toBe($folder->event->name)
+        ->and($event->tasks()->first()->name)->toBe($task->name);
 });
