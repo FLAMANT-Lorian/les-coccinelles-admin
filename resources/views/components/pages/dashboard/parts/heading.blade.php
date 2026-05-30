03f09bca41fd7125066a33e20e9a9128737bcb59
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2026-05-30 16:10:48 +0200

    fix permissions on dashboard

--- resources/views/components/pages/dashboard/parts/heading.blade.php
+++ resources/views/components/pages/dashboard/parts/heading.blade.php
@@ -1,22 +1,33 @@
+@php
+    use App\Models\Booking;
+    use App\Models\Meeting;
+    use App\Models\Event;
+@endphp
 <div class="col-span-full flex flex-col rl:flex-row rl:justify-between rl:items-center gap-4 rl:gap-8">
     <h1 class="text-3xl rg:text-4.5xl font-semibold">{{ __('pages/dashboard.heading') }}</h1>
     <div class="flex flex-col gap-4 rg:flex-row">
-        <x-pages.dashboard.fast-link
-            icon="#calendar"
-            color="#DBEAFE"
-            :url="route('bookings.create')"
-            :label="__('pages/dashboard.fast_links.1')"/>
+        @can('create', Booking::class)
+            <x-pages.dashboard.fast-link
+                icon="#calendar"
+                color="#DBEAFE"
+                :url="route('bookings.create')"
+                :label="__('pages/dashboard.fast_links.1')"/>
+        @endcan
 
-        <x-pages.dashboard.fast-link
-            icon="#meetings"
-            color="#E3F0E7"
-            :url="route('meetings', ['create' => true])"
-            :label="__('pages/dashboard.fast_links.2')"/>
+        @can('create', Meeting::class)
+            <x-pages.dashboard.fast-link
+                icon="#meetings"
+                color="#E3F0E7"
+                :url="route('meetings', ['create' => true])"
+                :label="__('pages/dashboard.fast_links.2')"/>
+        @endcan
 
-        <x-pages.dashboard.fast-link
-            icon="#events"
-            color="#E4DCF4"
-            :url="route('events.index', ['create' => true])"
-            :label="__('pages/dashboard.fast_links.3')"/>
+        @can('create', Event::class)
+            <x-pages.dashboard.fast-link
+                icon="#events"
+                color="#E4DCF4"
+                :url="route('events.index', ['create' => true])"
+                :label="__('pages/dashboard.fast_links.3')"/>
+        @endcan
     </div>
 </div>
