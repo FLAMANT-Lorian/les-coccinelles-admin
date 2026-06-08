f47f307332c5111a8e8ff75a3bcd62a9dae06e7f
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2026-06-08 21:47:48 +0200

    feat: Add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,13 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
+    {{-- FAVICON --}}
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/img/favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/img/favicon/favicon-16x16.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/img/favicon/favicon-32x32.png') }}">
+    <link rel="shortcut icon" href="{{ asset('assets/img/favicon/favicon.ico') }}">
+    <link rel="manifest" href="{{ asset('assets/img/favicon/site.webmanifest') }}">
+
 </head>
 <body x-data="{ modalOpen: false }"
       class="bg-white lg:flex lg:flex-row"
